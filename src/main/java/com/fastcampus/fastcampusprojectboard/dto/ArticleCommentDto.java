8b3438d4aff32e556461d0b8223acdf9da56fad6
mingang211 <a989977@naver.com>
2024-08-14 14:22:58 +0900

    #18 - 댓글 서비스 로직의 테스트와 골격 잡기
    댓글과 관련된 단순 crud 기능을 테스트로 표현

---
+++ src/main/java/com/fastcampus/fastcampusprojectboard/dto/ArticleCommentDto.java
@@ -0,0 +1,22 @@
+package com.fastcampus.fastcampusprojectboard.dto;
+
+import java.time.LocalDateTime;
+
+/**
+ * DTO for {@link com.fastcampus.fastcampusprojectboard.domain.ArticleComment}
+ */
+public record ArticleCommentDto(
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy,
+        String content
+) {
+  public static ArticleCommentDto of(LocalDateTime createdAt,
+                           String createdBy,
+                           LocalDateTime modifiedAt,
+                           String modifiedBy,
+                           String content) {
+    return new ArticleCommentDto(createdAt, createdBy, modifiedAt, modifiedBy, content);
+  }
+}
---
+++ src/main/java/com/fastcampus/fastcampusprojectboard/service/ArticleCommentService.java
@@ -0,0 +1,27 @@
+package com.fastcampus.fastcampusprojectboard.service;
+
+import com.fastcampus.fastcampusprojectboard.dto.ArticleCommentDto;
+import com.fastcampus.fastcampusprojectboard.repository.ArticleCommentRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@Service
+public class ArticleCommentService {
+
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public ArticleCommentService(ArticleCommentRepository articleCommentRepository) {
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComment(long articleId) {
+        return List.of();
+    }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+    }
+}
